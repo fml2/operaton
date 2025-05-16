2806e509658db1e85ae7caa3fd261c0eeabad372
Karsten Thoms <karsten.thoms@gmail.com>
2025-05-16 14:57:53 +0200

    make processInstanceIdNotIn default method, add Override annotation

--- engine/src/main/java/org/operaton/bpm/engine/impl/HistoricProcessInstanceQueryImpl.java
+++ engine/src/main/java/org/operaton/bpm/engine/impl/HistoricProcessInstanceQueryImpl.java
@@ -122,6 +122,7 @@ public class HistoricProcessInstanceQueryImpl extends AbstractVariableQueryImpl<
     return this;
   }
 
+  @Override
   public HistoricProcessInstanceQuery processInstanceIdNotIn(String... processInstanceIdNotIn){
     ensureNotNull("processInstanceIdNotIn", (Object[]) processInstanceIdNotIn);
     this.processInstanceIdNotIn = processInstanceIdNotIn;
